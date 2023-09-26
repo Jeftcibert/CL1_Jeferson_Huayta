cd87b87c4f498ca6d6dc6a73c24c94f38b47212e
Jeferson Huayta <Jeferson@gmail.com>
2023-09-25 22:44:17 -0500

    Agreamos el metodo paara listar los datos de nuestra bases de datos

--- src/main/java/org/cibecl1/controller/LaptopController.java
+++ src/main/java/org/cibecl1/controller/LaptopController.java
@@ -1,5 +1,21 @@
 package org.cibecl1.controller;
 
+import org.cibecl1.repository.ILaptopRpository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LaptopController {
+	
+	@Autowired
+	private ILaptopRpository repo;
+	@GetMapping("/listar")
+	public String listarLaptop(Model model) {
+		model.addAttribute("lstLaptop",repo.findAll());
+		return "Listado";
+	}
+	
 
 }
