ccbb84156d7102bd0076aa6011316a21a8ec13bd
Jeferson Huayta <Jeferson@gmail.com>
2023-09-25 22:38:16 -0500

    Agregamos la interfaz para hacer uso de los metodo JapRepository

---
+++ src/main/java/org/cibecl1/repository/ILaptopRpository.java
@@ -0,0 +1,8 @@
+package org.cibecl1.repository;
+
+import org.cibecl1.model.Laptop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILaptopRpository extends JpaRepository<Laptop, String> {
+
+}
