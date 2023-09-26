e568551d979538c17467dfec94de59c555a3f5ca
Jeferson Huayta <Jeferson@gmail.com>
2023-09-25 22:19:07 -0500

    creamos el proyecto donde se realizara la evaluacion

---
+++ src/main/java/org/cibecl1/Cl1JefersonHuaytaZeladaApplication.java
@@ -0,0 +1,13 @@
+package org.cibecl1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JefersonHuaytaZeladaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JefersonHuaytaZeladaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cibecl1/Cl1JefersonHuaytaZeladaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibecl1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JefersonHuaytaZeladaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
